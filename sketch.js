72f892f982930859747383b91d99d103a3d9161e
maryleneB99 <marylene3@hotmail.co.uk>
2024-12-10 16:24:11 +0000

    stars added, add some hearts

--- sketch.js
+++ sketch.js
@@ -2,9 +2,26 @@ function setup() {
   createCanvas(400, 400);
 }
 
-function draw() {
-  background(220);
-}
+  
+  function draw() {
+    background(220);
+  
+    drawStar(200,75);
+    drawStar(150,300);
+  
+  }
+  
+  function drawStar(tx,ty) {
+    push();
+    translate(tx,ty);
+    strokeWeight(2);
+    line(0-10,0,0+10,0);
+    line(0,0-10,0,0+10);
+    line(0-10,0-10,0+10,0+10);
+    line(0-10,0+10,0+10,0-10);
+    pop();
+  }
+
 
 
 //draw a star
